b86ba678c9c438e05ee7239ee1feb61c1422fc22
al.slapp <al.slapp@gmail.com>
2023-09-12 13:24:25 +0300

    Основные типы - Type Guard

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,62 @@
-// Основные типы - Null
-// при отключении настройки в tsconfig strictNullChecks,
-// в переменную с любым типом можно положить null
-// const n: null = null;
-// const n1: any = null;
-// const n2: number = null;
-// const n3: string = null;
-// const n4: boolean = null;
-// const n5: undefined = null;
+// Основные типы - Type Guard
 
+// type guard - это просто функция для проверки типа переменной
 interface IUser {
 	name: string;
+	email: string;
+	login: string;
 }
 
-function getUser() {
-	if (Math.random() > 0.5) {
-		return null;
+const user: IUser = {
+	name: 'Вася',
+	email: 'sdf@asdf.ru',
+	login: 'vasya'
+}
+
+interface IAdmin {
+	name: string;
+	role: number;
+}
+
+function logId(id: string | number) {
+
+	if (isString(id)) {
+		console.log(id);
 	}
 	else {
-		return {
-			name: 'Вася'
-		} as IUser
+		console.log(id);
 	}
 }
 
-const user = getUser();
-if (user) {
-	const n55 = user.name;
-	console.log(n55);
+function isString(x: string | number): x is string {
+	return typeof x === 'string';
+}
+
+/////////////////////////////////////////////////////
+
+// type guard - т.е. запись: user is IAdmin говорит о том, что возвращаем bool true или false - пользователь админ или нет
+// почему не возвращаем просто boolean - тогда ts не сужает тип user
+function isAdmin(user: IUser | IAdmin): user is IAdmin {
+	return 'role' in user; // user.hasOwnProperty('role');
+}
+
+// например в типе много вложенных свойств, можно кастануть и проверить наличие ключа
+function isAdminAlternative(user: IUser | IAdmin): user is IAdmin {
+	return (user as IAdmin).role !== undefined;
+}
+
+
+function setRole(user: IUser | IAdmin) {
+	if (isAdmin(user)) {
+		user.role = 0;
+	}
+	else {
+		throw new Error('Пользователь не Admin');
+	}
+}
+
+
+// сделать type guard на массив, в котором содержатся строки
+const isStringArray = (arg: any): arg is string[] => {
+	return Array.isArray(arg) && arg.every(item => typeof item === 'string')
 }
