bb65dbffdded410b314ff57b4d0da26fc0a6024e
al.slapp <al.slapp@gmail.com>
2023-09-12 11:57:22 +0300

    Основные типы - Unknown

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,48 @@
-type voidFn = () => void;
+// Основные типы - Unknown
 
-const f1: voidFn = () => {
-	return true;
+// unknow более строгий чем any
+// хорошо его использовать тогда, когда неизвестно, что будет в переменной (вместо any)
+
+let input: unknown;
+
+input = 3;
+input = ['dsd', 345];
+
+// тут ошибка, т.к. нужно сузить тип
+// let res: string = input;
+
+// сужение типов для unknown не работает, нужно каждый раз проверять тип переменной
+function run(i: unknown) {
+	if (typeof i == 'number') {
+		i++;
+	}
+	else {
+		i // тут i снова unknown
+	}
 }
-const b = f1();
 
-console.log(`b: `, b);
 
-// void позволяет игнорировать все что выводит функция,
-// это нужно для совместимости js функций например forEach и ts типизации
+// пример использования unknown
+async function getData() {
+	try {
+		await fetch('');
+	}
+	catch (error) {
+		// тут error имеет type unknown, т.е. нужно явно проверять каждый раз тип
+		if (error instanceof Error) {
+			console.log(error.message);
+		}
+
+		// или можно кастануть к типу, но соотв. в error может прийти что угодно, например
+		// строка и тогда код выдаст ошибку типа.
+		const e = error as Error;
+		console.log(e.message);
+
+	}
+}
+
+// union с unknown
+
+type U1 = unknown | number; // при юнион типах - U1 всегда будет unknown
+
+type T1 = unknown & string; // при intersection T1 всегда будет тип, которым расширяем, т.е. тут: T1 будет string
