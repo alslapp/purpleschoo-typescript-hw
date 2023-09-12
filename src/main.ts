c0adec828f7205340bd6592798527e8bb26114ce
al.slapp <al.slapp@gmail.com>
2023-09-12 13:03:49 +0300

    Основные типы - Null

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,30 @@
-// Основные типы - Never
+// Основные типы - Null
+// при отключении настройки в tsconfig strictNullChecks,
+// в переменную с любым типом можно положить null
+// const n: null = null;
+// const n1: any = null;
+// const n2: number = null;
+// const n3: string = null;
+// const n4: boolean = null;
+// const n5: undefined = null;
 
-// #1
-// тут использовать void некорректно, т.к. эта функция никогда
-// ничего не вернет, а void подразумевает, что может вернуться что угодно, 
-// но будет проигнорировано
-function generateError(message: string): never {
-	throw new Error(message);
+interface IUser {
+	name: string;
 }
 
-// #2
-// функция никогда не остановится
-function dumpError(): never {
-	while (true) { }
-}
-
-// #3
-// функция никогда не остановится
-function rec(): never {
-	return rec();
-}
-
-// #4
-type paymentAction = 'refund' | 'checkout' | 'reject';
-
-function processAction(action: paymentAction) {
-	switch (action) {
-		case 'refund':
-			// ...
-			break;
-		case 'checkout':
-			// ...
-			break;
-		// что тут происходит:
-		// в compile time: сюда никогда не дейдет действие, поэтому тут action имеет тип never
-		// но если вдруг, в run time добавиться новый action (например придет с api),
-		// соотв. switch додет сюда и выдаст ошибку
-		default:
-			// const _: never = action;
-			throw new Error(`Нет такого action`);
+function getUser() {
+	if (Math.random() > 0.5) {
+		return null;
+	}
+	else {
+		return {
+			name: 'Вася'
+		} as IUser
 	}
 }
 
-// #5
-
-// в рантайме реально может прийти что угодно, для этого
-// делаем  Исчерпывающую проверку
-function isString(x: string | number): boolean {
-	if (typeof x === 'string') {
-		return true;
-	}
-	else if (typeof x === 'number') {
-		return false;
-	}
-	// т.е. если x явно не строка и не число и если функцию необходим прервать
-	// это называется Исчерпывающая проверка, т.е. функция generateError
-	// возвращает never, соотв. ошибки в compile time и в run time не будет.
-	// обязательно функция generateError должна вернуть never, 
-	// как будто бы функция isString дальше generateError() не пройдет
-	// это нужно для типизации
-	generateError('Тип переменной неопределен');
+const user = getUser();
+if (user) {
+	const n55 = user.name;
+	console.log(n55);
 }
