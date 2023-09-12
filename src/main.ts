1c3193fe93bb03cc5c9ebeb5f113640887e713ce
al.slapp <al.slapp@gmail.com>
2023-09-12 10:50:56 +0300

    Упражнение - Типизируем ответ сервера

--- src/main.ts
+++ src/main.ts
@@ -1,122 +1,49 @@
-const makeOrdinal = (words: string): string => {
-	console.log(`fn makeOrdinal, words: ${words}`);
-	return "zero";
+enum PaymentStatus {
+	SUCCESS = 'success',
+	FAILED = 'failed',
 }
 
-const isFinite = (num: number): boolean => {
-	console.log(`fn isFinite, num: ${num}`);
-	return true;
+interface IPayment {
+	sum: number;
+	from: number;
+	to: number;
 }
 
-const isSafeNumber = (num: number): boolean => {
-	console.log(`fn isSafeNumber, num: ${num}`);
-	return true;
+interface IResponseData extends IPayment {
+	databaseId: number;
 }
 
-const TEN = 10;
-const ONE_HUNDRED = 100;
-const ONE_THOUSAND = 1000;
-const ONE_MILLION = 1000000;
-const ONE_BILLION = 1000000000;           //         1.000.000.000 (9)
-const ONE_TRILLION = 1000000000000;       //     1.000.000.000.000 (12)
-const ONE_QUADRILLION = 1000000000000000; // 1.000.000.000.000.000 (15)
-const MAX = 9007199254740992;             // 9.007.199.254.740.992 (15)
-
-const LESS_THAN_TWENTY = [
-	'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
-	'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-];
-
-const TENTHS_LESS_THAN_HUNDRED = [
-	'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
-];
-
-/**
- * Converts an integer into words.
- * If number is decimal, the decimals will be removed.
- * @example toWords(12) => 'twelve'
- * @param {number|string} number
- * @param {boolean} [asOrdinal] - Deprecated, use toWordsOrdinal() instead!
- * @returns {string}
- */
-function toWords(number: string, asOrdinal: boolean): string {
-	let words;
-	let num = parseInt(number, 10);
-
-	if (!isFinite(num)) {
-		throw new TypeError(
-			'Not a finite number: ' + number + ' (' + typeof number + ')'
-		);
-	}
-	if (!isSafeNumber(num)) {
-		throw new RangeError(
-			'Input is not a safe number, it’s either too large or too small.'
-		);
-	}
-	words = generateWords(num);
-	return asOrdinal ? makeOrdinal(words) : words;
+interface IResponseSuccess {
+	status: PaymentStatus.SUCCESS,
+	data: IResponseData;
 }
 
-function generateWords(number: number, ...rest: any[]): string {
-	let remainder = 0;
-	let word: string | undefined;
-	let words = rest?.[0] || [];
-
-	// We’re done
-	if (number === 0) {
-		return !words ? 'zero' : words.join(' ').replace(/,$/, '');
+interface IResponseFailed {
+	status: PaymentStatus.FAILED,
+	data: {
+		errorMessage: string,
+		errorCode: number;
 	}
-
-	// If negative, prepend “minus”
-	if (number < 0) {
-		words.push('minus');
-		number = Math.abs(number);
-	}
-
-	if (number < 20) {
-		word = LESS_THAN_TWENTY[number];
-	}
-	else if (number < ONE_HUNDRED) {
-		remainder = number % TEN;
-		word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
-		// In case of remainder, we need to handle it here to be able to add the “-”
-		if (remainder) {
-			word += '-' + LESS_THAN_TWENTY[remainder];
-			remainder = 0;
-		}
-	}
-	else if (number < ONE_THOUSAND) {
-		remainder = number % ONE_HUNDRED;
-		word = generateWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
-
-	}
-	else if (number < ONE_MILLION) {
-		remainder = number % ONE_THOUSAND;
-		word = generateWords(Math.floor(number / ONE_THOUSAND)) + ' thousand,';
-
-	}
-	else if (number < ONE_BILLION) {
-		remainder = number % ONE_MILLION;
-		word = generateWords(Math.floor(number / ONE_MILLION)) + ' million,';
-
-	}
-	else if (number < ONE_TRILLION) {
-		remainder = number % ONE_BILLION;
-		word = generateWords(Math.floor(number / ONE_BILLION)) + ' billion,';
-
-	}
-	else if (number < ONE_QUADRILLION) {
-		remainder = number % ONE_TRILLION;
-		word = generateWords(Math.floor(number / ONE_TRILLION)) + ' trillion,';
-
-	}
-	else if (number <= MAX) {
-		remainder = number % ONE_QUADRILLION;
-		word = generateWords(Math.floor(number / ONE_QUADRILLION)) + ' quadrillion,';
-	}
-
-	words.push(word);
-	return generateWords(remainder, words);
 }
 
-export { toWords }
+
+const getServerData = async (payment: IPayment): Promise<IResponseSuccess | IResponseFailed> => {
+	const res = await fetch(`/api/get/data`, {
+		method: 'GET',
+		body: JSON.stringify(payment),
+	});
+	return await res.json();
+}
+
+
+// Запрос в виде платежа
+const payment: IPayment = {
+	sum: 10000,
+	from: 2,
+	to: 4,
+};
+
+(async () => {
+	const data = await getServerData(payment);
+	console.log(`Ответ от сервера: `, data);
+})();
