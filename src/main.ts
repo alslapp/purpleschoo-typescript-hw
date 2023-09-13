b09e43e202312b26b369fcdbd152a54a68fc7abd
al.slapp <al.slapp@gmail.com>
2023-09-13 14:04:47 +0300

    Классы - Упражнение - Перегрузка методов

--- src/main.ts
+++ src/main.ts
@@ -1,90 +1,47 @@
 // Классы
-// урок 2
-// class User {
-// 	name!: string; // ! - обозначить для ts, что я понимаю, что делаю, чтобы убрать ошибку, если strictPropertyInitialization стоит в true
-// }
+// урок 5 Упражнение - Перегрузка методов
 
-// const user = new User();
-// user.name = 'Вася';
-// console.log({
-// 	user,
-// });
+class User {
+	skills: string[] = [];
 
-//////////////////////////////////////
-// урок 3 Конструктор
-// перегрузка конструктора
-// перегрузки нужно прописывать только для ide, чтобы подсвечивались эти самые перегрузки, в рантайме их нет
+	private addSkill(skill: string): void {
+		const skillIndex = this.skills.findIndex((s) => s === skill);
+		if (skillIndex < 0) this.skills.push(skill);
+	}
 
-// class User {
-// 	name: string;
-// 	age: number;
+	addSkills(skill: string): void;
+	addSkills(skills: string[]): void;
+	addSkills(skillOrSkills: string | string[]): void {
+		if (typeof skillOrSkills === 'string') {
+			this.addSkill(skillOrSkills);
+		} else if (Array.isArray(skillOrSkills)) {
+			skillOrSkills.map(this.addSkill.bind(this));
+		}
+	}
+}
 
-// 	constructor();
-// 	constructor(age: number);
-// 	constructor(name: string);
-// 	constructor(name: string, age: number);
+const user = new User();
 
-// 	// последний конструктор должен удовлетворять всем перегрузкам
-// 	constructor(ageOrname?: string | number, age?: number) {
-// 		if (typeof ageOrname === 'string') {
-// 			this.name = ageOrname;
-// 		} else if (typeof ageOrname === 'number') {
-// 			this.age = ageOrname;
-// 		}
+user.addSkills('dev');
+console.log(user);
 
-// 		if (typeof age === 'number') {
-// 			this.age = age;
-// 		}
-// 	}
-// }
+user.addSkills(['dev', 'devOps', 'front']);
+console.log(user);
 
-// const user = new User('Вася');
-// const user2 = new User();
-// const user3 = new User(35);
-// const user4 = new User('Вася', 35);
-// console.log({
-// 	user,
-// 	user2,
-// 	user3,
-// 	user4,
-// });
+user.addSkills('dev');
+console.log(user);
 
-//////////////////////////////////////
-// урок 4 Методы класса
+///////////////////////////////
+// перегрузка функции
+function run(distance: string): string;
+function run(distance: number): number;
+function run(distance: number | string): number | string {
+	if (typeof distance == 'number') {
+		return 1;
+	} else {
+		return '1';
+	}
+}
 
-// enum PaymentStatus {
-// 	Holded, // деньги забронированы, но не списаны
-// 	Processed, // деньги списаны
-// 	Reversed, // Holded отменяем, т.е. деньги на счету разблокируются
-// }
-
-// class Payment {
-// 	id: number;
-// 	status: PaymentStatus = PaymentStatus.Holded;
-// 	createdAt: Date = new Date();
-// 	updatedAt: Date;
-
-// 	constructor(id: number) {
-// 		this.id = id;
-// 	}
-
-// 	getPaymentLifeTime(): number {
-// 		return new Date().getTime() - this.createdAt.getTime();
-// 	}
-
-// 	unHoldPayment(): void {
-// 		if (this.status == PaymentStatus.Processed) {
-// 			throw new Error('Платёж не может быть возвращён');
-// 		}
-// 		this.status = PaymentStatus.Reversed;
-// 		this.updatedAt = new Date();
-// 	}
-// }
-
-// const payment = new Payment(1);
-
-// payment.unHoldPayment();
-// console.log(payment);
-
-// const time = payment.getPaymentLifeTime();
-// console.log(time);
+run(1);
+run('1');
