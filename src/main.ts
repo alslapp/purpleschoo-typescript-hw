92f4c55eab4e51465becb15d1ef23c9dd7b7f8b5
al.slapp <al.slapp@gmail.com>
2023-09-17 16:31:36 +0300

    Классы - урок Статические свойства

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 // Классы
-// урок
-type PaymentStatus = 'new' | 'paid';
+// урок Статические свойства
 
-class Payment {
-	id: number;
-	status: PaymentStatus
+class UserService {
+
+	// static name: string = 'name' //статическое свойство name нельзя определить, т.к. оно существует под капотом класса
+	// можно определить только обычное свойство name
+
+	static db: any;
+
+	static getUser() {
+		return this.db.findById(); // или вместо UserService. можно использовать this.
+	}
+
+	constructor(id: number) {
+
+	}
+
+	create() {
+		UserService.db;
+	}
+
+	// это что то типа constructor только для статического выполнения
+	// можно использовать только синхронные вызовы
+	static {
+		UserService.db = {
+			findById: (): void => { }
+		}
+	}
+
 }
+
+UserService.db;
+
+const inst = new UserService();
+inst.create();
