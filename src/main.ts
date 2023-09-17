98684b9b7b1ec74f2e268d0df3f143be473ab2d7
al.slapp <al.slapp@gmail.com>
2023-09-17 19:02:44 +0300

    Классы - урок Типизация this

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,32 @@
 // Классы
-// урок Работа с this
+// урок Типизация this
 
-class Payment {
-	private date: Date = new Date();
+class UserBuilder {
+	name: string;
 
-	getDate(this: Payment, test: string) { // this на первом месте - это фича TS - чтобы при потере контекста подсветить ошибку, в билде параметра this не будет
-
-		console.log('getDate', test);
-
-		return this.date;
+	setName(name: string): this {
+		this.name = name;
+		return this;
 	}
 
-	getDateArrow = () => {
-		// при таком определении метода ( именно через = "равно" !!! ) не нужно биндить контекст и передавать this в параметрах метода 
-		// (если присваивать метод свойсту объекта например: user = { test: (new Payment().getDateArrow) } ),
-		// т.к. такое создание метода не создает метод в прототипе, а создает его на лету, при инстанцировании класса, т.е. такая запись в райнтайме выглядите так:
-		/*
-			constructor() {
-				this.getDateArrow = () => {
-					console.log('getDateArrow');
-					return this.date;
-				};
-			}
-		*/
-		console.log('getDateArrow');
-		return this.date;
+	isAdmin(): this is AdminBuilder {
+		return this instanceof AdminBuilder;
 	}
 
 }
 
-const p = new Payment();
-
-const user = {
-	id: 1,
-	paymentDate: p.getDate.bind(p),
-	paymentDateArrow: p.getDateArrow
+class AdminBuilder extends UserBuilder {
+	roles: string[] = ['user', 'admin']; // если у классов не будет разных методов или свойств, то в рантайме невозможно будет отличить UserBuilder от AdminBuilder и type guard определит тип как: let user: UserBuilder | AdminBuilder;
 }
 
-// console.log(p.getDate('1'));
-// console.log(user.paymentDate('4'));
-// console.log(user.paymentDateArrow());
+const res = new UserBuilder().setName('вася');
+const res2 = new AdminBuilder().setName('вася');
 
+let user: UserBuilder | AdminBuilder = new UserBuilder();
 
-class PaymentPersistent extends Payment {
-	// save() {
-	// 	return super.getDate('test');
-	// }
-	// saveThis() {
-	// 	return this.getDate('test this');
-	// }
-
-	saveArrow() {
-		return super.getDateArrow(); // super - так не работает, потому что super вызывает меторы из прототипа, 
-		// а созданеие метода через = и стрелочную функцию не создают метод в прототипе, 
-		// а создаю метод в конструкторе, при инстанцировании класса
-	}
-
-	saveArrowThis() {
-		return this.getDateArrow(); // this - так работает
-	}
-
+if (user.isAdmin()) {
+	console.log(user);
 }
-
-const pp = new PaymentPersistent();
-// console.log(pp.save());
-// console.log(pp.saveArrow());
-console.log(pp.saveArrowThis());
+else {
+	console.log(user);
+}
