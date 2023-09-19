7f34243bc2e43965fae7dfd650173b4e0d428e9d
al.slapp <al.slapp@gmail.com>
2023-09-19 21:40:10 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 // Generics
 // Домашнее задание - Generics
 
+// Нужно поменять местами ключи и значения в объекте
+
 type TObj = Record<string, number>;
+type TObjSwap = Record<number, string>;
 
 const obj: TObj = {
 	a: 1,
 	b: 2,
 };
 
-function swapKeysAndValues<T extends TObj>(obj: T) {
-	const res: Record<number, string> = {};
+function swapKeysAndValues<T extends TObj, S extends TObjSwap>(obj: T): S {
+	const res = <S>{};
 	Object.entries(obj).forEach(([key, value]) => {
 		res[value] = key
 	})
 	return res;
 }
 
-const res = swapKeysAndValues<TObj>(obj);
+const res = swapKeysAndValues(obj);
 
 console.log(res);
