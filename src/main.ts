38bf298093b8537a573e253f68dd14cf1492e9ed
al.slapp <al.slapp@gmail.com>
2023-09-19 15:27:22 +0300

    Generics - Упражнение - Функция сортировки id

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,73 @@
 // Generics
-// Упражнение - Функция преобразования в строку
+// Упражнение - Функция сортировки id
 
 /*
-Необходимо написать функцию toString, которая принимает любой тип 
-и возвращает его строковое представление. Если не может, то возвращает undefined
+Необходимо написать функцию сортировки любых объектов,
+которые имют id по убыванию и по возрастанию
 */
 
+const data = [
+	{ id: 1, name: 'Вася', },
+	{ id: 2, name: 'Петя', },
+	{ id: 3, name: 'Надя', },
+	{ id: 4, name: 'Зина', },
+];
 
-function toString<T>(a: T): string | undefined {
-
-	console.log('type', typeof a);
-
-	if (Array.isArray(a)) {
-		return a.toString();
-	}
-
-	switch (typeof a) {
-		case 'string':
-			return a;
-
-		case 'number':
-		case 'symbol':
-		case 'bigint':
-		case 'boolean':
-		case 'function':
-			return a.toString();
-
-		case 'object':
-			return JSON.stringify(a);
-
-		default:
-			return undefined;
-	}
+interface Id {
+	id: number;
 }
 
-console.log(toString('Привет'));
-console.log(toString('1111'));
-console.log(toString([1, 2, 'asdfasdf', { test: 'adfasdf' }]));
-console.log(toString({ test: 'adfasdf' }));
+// моё решение
+enum Directions {
+	ASC = 'asc',
+	DESC = 'desc',
+}
 
-console.log(toString(true));
+// function sortAsc<T extends Id>(a: T, b: T): number {
+// 	if (a.id < b.id) {
+// 		return -1;
+// 	}
+// 	else if (a.id > b.id) {
+// 		return 1;
+// 	}
+// 	return 0;
+// }
 
-console.log(toString(() => { }));
+// function sortDesc<T extends Id>(a: T, b: T): number {
+// 	if (a.id > b.id) {
+// 		return -1;
+// 	}
+// 	else if (a.id < b.id) {
+// 		return 1;
+// 	}
+// 	return 0;
+// }
 
-console.log(toString(function () { }));
+// function sortById<T extends Id>(data: Array<T>, direction: Directions = Directions.ASC): Array<T> { // вместо дженерика можно передать/вернуть как: T[]
+// 	switch (direction) {
+// 		case Directions.ASC:
+// 			return data.sort(sortAsc);
+// 		case Directions.DESC:
+// 			return data.sort(sortDesc);
+// 	}
+// }
+
+// решение Антона
+// <!--
+function sortById<T extends Id>(data: T[], direction: Directions = Directions.ASC): T[] {
+	return data.sort((a, b) => {
+		switch (direction) {
+			case Directions.ASC:
+				return a.id - b.id;
+			case Directions.DESC:
+				return b.id - a.id;
+		}
+	});
+}
+// -->
+
+
+
+
+console.log(sortById(data, Directions.DESC));
+console.log(sortById(data));
