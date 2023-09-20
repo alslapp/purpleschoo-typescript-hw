611c8f2867d9646484e3cf4cba5c1d55dd320655
al.slapp <al.slapp@gmail.com>
2023-09-20 06:38:44 +0300

    Манипуляция с типами

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,23 @@
-// Generics
-// урок Mixins
+// Манипуляция с типами
+// урок
 
-type Constructor = new (...args: any[]) => {};
-type GConstructor<T = {}> = new (...args: any[]) => T; // задать тип по умолчанию: <T = {}>
-
-class List {
-	constructor(public items: string[]) { }
+interface IUser {
+	name: string;
+	age: number;
 }
 
-class Accordeon {
-	isOpened: boolean;
+type TKeysOfUser = keyof IUser;
+
+const key: TKeysOfUser = 'age';
+
+
+function getValue<T extends IUser, K extends keyof T>(obj: T, key: K) {
+	return obj[key];
 }
 
-class ExtendedListClass extends List {
-	first() {
-		return this.items[0];
-	}
-}
+const user: IUser = {
+	name: 'Вася',
+	age: 30,
+};
 
-////////////////
-const res = new ExtendedListClass(['1', '2']);
-console.log(res.first());
-////////////////
-
-
-type ListType = GConstructor<List>;
-type AccordeonType = GConstructor<Accordeon>;
-
-// это миксин:
-function ExtendsList<TBase extends ListType & AccordeonType>(Base: TBase) {
-	return class ExtendedList extends Base {
-		first() {
-			return this.items[0];
-		}
-	}
-}
-
-
-class AccordeonList {
-	isOpened: boolean;
-	constructor(public items: string[]) { }
-}
-
-
-const list = ExtendsList(AccordeonList);
-const res2 = new list(['first', 'second']);
-console.log(res2.first());
-
-
-
-// пример про дженерик конструктор: https://www.simonholywell.com/post/typescript-constructor-type.html
+const userName = getValue(user, 'age');
