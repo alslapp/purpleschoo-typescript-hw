8f41ac8a5e2e57efff923db6861ea5a2a813108c
al.slapp <al.slapp@gmail.com>
2023-09-20 08:32:45 +0300

    Манипуляция с типами - Упражнение - Пишем функцию группировки

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,66 @@
 // Манипуляция с типами
-// урок
+// Упражнение - Пишем функцию группировки
 
-interface IUser {
-	name: string;
-	age: number;
+/*
+Необходимо написать функцию группировки, которая принимает массив объектов
+и его ключ, производит группировку по указанному ключу и возвращает
+сгруппированный объект.
+
+пример:
+
+const obj = [
+	{ group: 1, name: 'c', },
+	{ group: 1, name: 'r', },
+	{ group: 2, name: 't', },
+	{ group: 3, name: 'y', },
+	{ group: 4, name: 'u', },
+];
+
+При группировке по group:
+
+{
+	'1': [ { group: 1, name: 'c', }, { group: 1, name: 'r', },],
+	'2': [ { group: 2, name: 't', }, ],
+	'3': [ { group: 3, name: 'y', }, ],
+	'4': [ { group: 4, name: 'u', }, ],
 }
 
-type TKeysOfUser = keyof IUser;
 
-const key: TKeysOfUser = 'age';
+*/
 
-
-function getValue<T extends IUser, K extends keyof T>(obj: T, key: K) {
-	return obj[key];
+interface IData {
+	group: string | number;
+	name: string | number;
 }
 
-const user: IUser = {
-	name: 'Вася',
-	age: 30,
-};
+const obj1: IData[] = [
+	{ group: 1, name: 'c', },
+	{ group: 1, name: 'r', },
+	{ group: 2, name: 't', },
+	{ group: 3, name: 'y', },
+	{ group: 4, name: 'u', },
+];
 
-const userName = getValue(user, 'age');
+
+interface IGroup<T> {
+	[k: string]: T[];
+}
+
+type TKey = string | number | symbol;
+
+function group<T extends Record<TKey, any>>(array: T[], key: keyof T): IGroup<T> {
+	return array.reduce<IGroup<T>>((map: IGroup<T>, item: T) => {
+		const itemKey = item[key];
+		let curEl = map[itemKey];
+		if (Array.isArray(curEl)) {
+			curEl.push(item);
+		}
+		else {
+			curEl = [item];
+		}
+		map[itemKey] = curEl;
+		return map;
+	}, {});
+}
+
+console.log(group(obj1, 'group'));
