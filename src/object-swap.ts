ae2dce686eab2fd259c37489d2e724e21b4782dc
al.slapp <al.slapp@gmail.com>
2023-09-20 09:59:42 +0300

    работа надо ошибками

--- src/object-swap.ts
+++ src/object-swap.ts
@@ -3,22 +3,23 @@
 
 // Нужно поменять местами ключи и значения в объекте
 
-type TObj = Record<string, number>;
-type TObjSwap = Record<number, string>;
+type TKey = string | number | symbol;
+type TObj = Record<TKey, any>;
 
 const obj: TObj = {
 	a: 1,
 	b: 2,
 };
 
-function swapKeysAndValues<T extends TObj, S extends TObjSwap>(obj: T): S {
-	const res = <S>{};
+function swapKeysAndValues(obj: TObj): TObj {
+	const res: TObj = {};
 	Object.entries(obj).forEach(([key, value]) => {
 		res[value] = key
-	})
+	});
 	return res;
 }
 
 const res = swapKeysAndValues(obj);
 
+console.log(obj);
 console.log(res);
