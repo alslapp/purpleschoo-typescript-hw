41cfc69085172a3953db5563f9c2c375fbdff231
al.slapp <al.slapp@gmail.com>
2023-09-23 11:56:54 +0300

    upd: добавлен тип функции

--- src/11-sort-by/types.d.ts
+++ src/11-sort-by/types.d.ts
@@ -1,3 +1,3 @@
 declare module 'sort-by' {
-	export default function sortBy<T extends {}>(...args: Array<keyof T>): any;
+	export default function sortBy<T extends {}>(...args: Array<keyof T | ((key: string, value: any) => any)>): (a: T, b: T) => number;
 }
